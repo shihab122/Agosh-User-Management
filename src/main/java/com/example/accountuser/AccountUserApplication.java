43171d495abe5fd2c90a7057ef2d8747a22f7945
Shihab Hossain <shihabhossain611@gmail.com>
2022-06-22 21:26:21 +0600

    Intial Commit

---
+++ src/main/java/com/example/accountuser/AccountUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.accountuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/accountuser/AccountUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accountuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
